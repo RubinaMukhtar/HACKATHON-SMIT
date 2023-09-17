2a92bd7bea34164ba2072c90ace68074e2ac623e
RubinaMukhtar <muhammad921shahbaz@gmail.com>
2023-09-17 11:26:27 +0500

    HACKATHON-SMIT

---
+++ attendance hackathon/index.js
@@ -0,0 +1,20 @@
+
+ 
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
+ 
+   Your web app's Firebase configuration
+   For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCdFyjM4wTb7IKsSx2j03ZrLq45nWYAEsk",
+    authDomain: "attendance-application-9c8d6.firebaseapp.com",
+    projectId: "attendance-application-9c8d6",
+    storageBucket: "attendance-application-9c8d6.appspot.com",
+    messagingSenderId: "504404842298",
+    appId: "1:504404842298:web:70bc3a570ddb70c2a9a203",
+    measurementId: "G-GD11N4TT2G"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
